78731ab8540a9861b2ed5805b2fb0b5b937dd128
tabur <tabur@DERNOVAPI>
2022-12-09 22:31:00 +0300

    Добавьте файлы проекта.

---
+++ pract 7/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wut7Practa
+{
+    public class ArrowMenu
+    {
+        int selected;
+        int lenght;
+        int offset;
+        static string indent = "   ";
+        List<MyFile> buttons;
+        public ArrowMenu(List<MyFile> buttons, int offset = 0)
+        {
+            this.buttons = buttons;
+            this.lenght = offset + buttons.Count - 1;
+            this.offset = offset;
+            this.selected = offset;
+        }
+        private static void RemoveCursor(int y)
+        {
+            Console.SetCursorPosition(0, y);
+            Console.Write(indent);
+        }
+        public int Select()
+        {
+            ShowInfo.ShowFiles(buttons, indent);
+            ConsoleKey key = new ConsoleKey();
+            while (key != ConsoleKey.Enter)
+            {
+                Console.SetCursorPosition(0, selected);
+                Console.Write("->");
+                key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.Escape:
+                        return -1;
+                    case ConsoleKey.Enter:
+                        break;
+                    case ConsoleKey.UpArrow:
+                        RemoveCursor(selected);
+                        if (selected == offset)
+                            selected = lenght;
+                        else
+                            selected -= 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        RemoveCursor(selected);
+                        if (selected == lenght)
+                            selected = offset;
+                        else
+                            selected += 1;
+                        break;
+                }
+            }
+            return selected - offset;
+        }
+    }
+
+    public static class ShowInfo
+    {
+        public static void ShowFiles(List<MyFile> files, string offset)
+        {
+            string text = "";
+            foreach (MyFile file in files)
+            {
+                if (Directory.Exists(file.Path))
+                    text += offset + file.Name + "\\\n";
+                else
+                    text += offset + file.Name + "\n";
+            }
+            Console.WriteLine(text);
+        }
+    }
+    public class MyFile
+    {
+        public MyFile(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+    public class FileWorking
+    {
+        private static List<string> GetFiles(string path)
+        {
+            List<string> files = new List<string>();
+            foreach (string folder in Directory.GetDirectories(path))
+                files.Add(folder);
+
+            foreach (string file in Directory.GetFiles(path))
+                files.Add(file);
+
+            return files;
+        }
+        public static List<MyFile> LoadFolder(string path)
+        {
+            List<MyFile> files = new List<MyFile>();
+            foreach (string file in GetFiles(path))
+                files.Add(new MyFile(Path.GetFileName(file), file));
+            return files;
+        }
+    }
+}
---
+++ pract 7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace wut7Practa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myPath = "";
+            string oldPath;
+            List<MyFile> myFiles = new List<MyFile>();
+            while (true)
+            {
+                Console.Clear();
+                if (myPath == "")
+                {
+                    Console.WriteLine("Выбор диска");
+                    List<MyFile> drivesList = new List<MyFile>();
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        decimal AvailableFreeSpace = drive.AvailableFreeSpace / 1024 / 1024 / 1024;
+                        AvailableFreeSpace = Math.Round(AvailableFreeSpace, 1);
+                        MyFile myDrive = new MyFile($"{drive.Name} - Доступно {AvailableFreeSpace} гб", drive.Name);
+                        drivesList.Add(myDrive);
+                    }
+                    ArrowMenu myFileMenu = new ArrowMenu(drivesList, 1);
+                    int selected = myFileMenu.Select();
+                    if (selected == -1)
+                    {
+                        Environment.Exit(0);
+                    }
+                    myPath = drivesList[selected].Path;
+                }
+                else
+                {
+                    DirectoryInfo ParentFolder = Directory.GetParent(myPath);
+                    if (ParentFolder == null)
+                        oldPath = "";
+                    else
+                        oldPath = ParentFolder.ToString();
+                    Console.WriteLine(myPath);
+                    myFiles = FileWorking.LoadFolder(myPath);
+                    ArrowMenu myFileMenu = new ArrowMenu(myFiles, 1);
+                    int selected = myFileMenu.Select();
+                    if (selected == -1)
+                    {
+                        myPath = oldPath;
+                        continue;
+                    }
+                    string selected_path = myFiles[selected].Path;
+                    if (Directory.Exists(selected_path))
+                        myPath = selected_path;
+                    else
+                        try
+                        {
+                            Process.Start(selected_path);
+                        }
+                        catch
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Error while opening file");
+                            Thread.Sleep(2000);
+                        }
+                }
+            }
+        }
+    }
+}
